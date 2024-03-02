c9caa6d849e9ae12766099fa04837da90033aefa
Nerwyn Singh <nerwyn1994@gmail.com>
2024-03-02 15:19:08 -0500

    see if reference can be used instead of copy of theme since card-mod theming requires card-mod-theme value to be the same

--- .githooks/pre-commit.py
+++ .githooks/pre-commit.py
@@ -3,7 +3,7 @@ import ruamel.yaml
 
 theme_file_path = './themes/material_rounded.yaml'
 
-ruamel.yaml.representer.RoundTripRepresenter.ignore_aliases = lambda x, y: True
+# ruamel.yaml.representer.RoundTripRepresenter.ignore_aliases = lambda x, y: True
 yaml = ruamel.yaml.YAML()
 yaml.indent(mapping=4, sequence=4, offset=4)
 
@@ -11,10 +11,13 @@ with open(theme_file_path, 'r') as f:
 	theme_file = yaml.load(f)
 
 	# Create a no mod copy of theme which doesn't have card-mod keys
-	del theme_file['Material Rounded No Mod']
-	theme_file['Material Rounded No Mod'] = copy.deepcopy(theme_file['Material Rounded'])
-	del theme_file['Material Rounded No Mod']['card-mod-theme']
-	del theme_file['Material Rounded No Mod']['card-mod-root-yaml']
+	theme_title = 'Material Rounded No Mod'
+	del theme_file[theme_title]
+	# theme_file[theme_title] = copy.deepcopy(theme_file['Material Rounded'])
+	# del theme_file[theme_title]['card-mod-theme']
+	# del theme_file[theme_title]['card-mod-root-yaml']
+	theme_file[theme_title] = theme_file['Material Rounded']
+
 
 with open(theme_file_path, 'w') as f:
 	yaml.dump(theme_file, f)
