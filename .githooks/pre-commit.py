ae292ef99b3565104b44cf3080b0c0516c2f5576
Nerwyn Singh <nerwyn1994@gmail.com>
2024-04-28 03:38:24 -0400

    add transparent background no mod version of theme

--- .githooks/pre-commit.py
+++ .githooks/pre-commit.py
@@ -22,15 +22,15 @@ with open(theme_file_path, 'r') as f:
 	theme_title = 'Material Rounded Transparent Card'
 	transparent = 'var(--token-color-background-base)'
 	del theme_file[theme_title]
-	theme_file[theme_title] = copy.deepcopy(theme_file['Material Rounded No Mod'])
+	theme_file[theme_title] = copy.deepcopy(theme_file['Material Rounded'])
 	theme_file[theme_title]['card-background-color'] = transparent
 	theme_file[theme_title]['ha-card-background'] = transparent
-	theme_file[theme_title]['ha-card-border-color'] = transparent\
+	theme_file[theme_title]['ha-card-border-color'] = transparent
 	
 	# Create a transparent card background version of theme with no card-mod
 	theme_title = 'Material Rounded Transparent Card No Mod'
 	del theme_file[theme_title]
-	theme_file[theme_title] = copy.deepcopy(theme_file['Material Rounded'])
+	theme_file[theme_title] = copy.deepcopy(theme_file['Material Rounded Transparent Card'])
 	del theme_file[theme_title]['card-mod-theme']
 	del theme_file[theme_title]['card-mod-root-yaml']
 
