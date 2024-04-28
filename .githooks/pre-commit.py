1415765ef1670c1f673b41a74babf0a3db85ce9d
Nerwyn Singh <nerwyn1994@gmail.com>
2024-04-28 03:35:57 -0400

    add transparent background no mod version of theme

--- .githooks/pre-commit.py
+++ .githooks/pre-commit.py
@@ -10,7 +10,7 @@ yaml.indent(mapping=4, sequence=4, offset=4)
 with open(theme_file_path, 'r') as f:
 	theme_file = yaml.load(f)
 
-	# Create a no mod copy of theme which doesn't have card-mod keys
+	# Create a no mod version of theme with no card-mod
 	theme_title = 'Material Rounded No Mod'
 	del theme_file[theme_title]
 	theme_file[theme_title] = copy.deepcopy(theme_file['Material Rounded'])
@@ -18,13 +18,21 @@ with open(theme_file_path, 'r') as f:
 	del theme_file[theme_title]['card-mod-root-yaml']
 
 	# Create a transparent card background version of theme
+	theme_file = yaml.load(f)
 	theme_title = 'Material Rounded Transparent Card'
 	transparent = 'var(--token-color-background-base)'
 	del theme_file[theme_title]
 	theme_file[theme_title] = copy.deepcopy(theme_file['Material Rounded No Mod'])
 	theme_file[theme_title]['card-background-color'] = transparent
 	theme_file[theme_title]['ha-card-background'] = transparent
-	theme_file[theme_title]['ha-card-border-color'] = transparent
+	theme_file[theme_title]['ha-card-border-color'] = transparent\
+	
+	# Create a transparent card background version of theme with no card-mod
+	theme_title = 'Material Rounded Transparent Card No Mod'
+	del theme_file[theme_title]
+	theme_file[theme_title] = copy.deepcopy(theme_file['Material Rounded'])
+	del theme_file[theme_title]['card-mod-theme']
+	del theme_file[theme_title]['card-mod-root-yaml']
 
 
 with open(theme_file_path, 'w') as f:
