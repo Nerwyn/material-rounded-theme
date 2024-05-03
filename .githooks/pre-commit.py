498feaebfd9c106f851224e875eeee80ba1f0090
Nerwyn Singh <nerwyn1994@gmail.com>
2024-05-03 14:22:25 -0400

    copying changes/fixes from v2 - do not use tokens but assign css vars directly, use padding instead of margins for view, fix navbar and toolbar mobile app colors

--- .githooks/pre-commit.py
+++ .githooks/pre-commit.py
@@ -8,20 +8,23 @@ yaml = ruamel.yaml.YAML()
 yaml.indent(mapping=4, sequence=4, offset=4)
 
 with open(theme_file_path, 'r') as f:
+	# Copy base theme and remove redundant card-mod fields
 	theme_file = yaml.load(f)
 	original_theme = copy.deepcopy(theme_file['Material Rounded'])
+	del original_theme['card-mod-root-yaml']
+	del original_theme['card-mod-more-info-yaml']
 
 	# Create a no mod version of theme with no card-mod
 	theme_title = 'Material Rounded No Mod'
-	del theme_file[theme_title]
+	theme_file.pop(theme_title, None)
 	theme_file[theme_title] = copy.deepcopy(original_theme)
 	del theme_file[theme_title]['card-mod-theme']
-	del theme_file[theme_title]['card-mod-root-yaml']
+	theme_file[theme_title]['app-header-background-color'] = 'var(--primary-background-color)'
 
 	# Create a transparent card background version of theme
 	theme_title = 'Material Rounded Transparent Card'
-	transparent = 'var(--token-color-background-base)'
-	del theme_file[theme_title]
+	transparent = 'var(--lovelace-background)'
+	theme_file.pop(theme_title, None)
 	theme_file[theme_title] = copy.deepcopy(original_theme)
 	theme_file[theme_title]['card-background-color'] = transparent
 	theme_file[theme_title]['ha-card-background'] = transparent
@@ -29,10 +32,10 @@ with open(theme_file_path, 'r') as f:
 	
 	# Create a transparent card background version of theme with no card-mod
 	theme_title = 'Material Rounded Transparent Card No Mod'
-	del theme_file[theme_title]
+	theme_file.pop(theme_title, None)
 	theme_file[theme_title] = copy.deepcopy(original_theme)
 	del theme_file[theme_title]['card-mod-theme']
-	del theme_file[theme_title]['card-mod-root-yaml']
+	theme_file[theme_title]['app-header-background-color'] = 'var(--primary-background-color)'
 	theme_file[theme_title]['card-background-color'] = transparent
 	theme_file[theme_title]['ha-card-background'] = transparent
 	theme_file[theme_title]['ha-card-border-color'] = transparent
