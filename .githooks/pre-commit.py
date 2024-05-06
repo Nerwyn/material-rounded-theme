8ffffeb664e1487971d66e40fe6bb38f6d883924
Nerwyn Singh <nerwyn1994@gmail.com>
2024-05-05 22:24:34 -0400

    update precommit to remove new card-mod-*-yaml keys

--- .githooks/pre-commit.py
+++ .githooks/pre-commit.py
@@ -12,6 +12,8 @@ with open(theme_file_path, 'r') as f:
 	theme_file = yaml.load(f)
 	original_theme = copy.deepcopy(theme_file['Material Rounded'])
 	del original_theme['card-mod-root-yaml']
+	del original_theme['card-mod-card-yaml']
+	del original_theme['card-mod-row-yaml']
 
 	# Create a no mod version of theme with no card-mod
 	theme_title = 'Material Rounded No Mod'
