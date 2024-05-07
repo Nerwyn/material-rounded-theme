6e0621f21a5c4b06fed35ff918c635dd5abbb2fa
Nerwyn Singh <nerwyn1994@gmail.com>
2024-05-07 02:16:58 -0400

    create source files from which theme file is generated, reducing theme file size and repetitive code

--- .githooks/pre-commit.py
+++ .githooks/pre-commit.py
@@ -1,51 +1,45 @@
 import copy
 import ruamel.yaml
 
-theme_file_path = './themes/material_rounded.yaml'
+yaml = ruamel.yaml.YAML(typ='safe')
 
-ruamel.yaml.representer.RoundTripRepresenter.ignore_aliases = lambda x, y: True
-yaml = ruamel.yaml.YAML()
-yaml.indent(mapping=4, sequence=4, offset=4)
+with open('./themes/material_rounded.yaml', 'w') as dist:
+	output = {}
 
-with open(theme_file_path, 'r') as f:
-	# Copy base theme and remove redundant card-mod fields
-	theme_file = yaml.load(f)
-	original_theme = copy.deepcopy(theme_file['Material Rounded'])
-	del original_theme['card-mod-root-yaml']
-	del original_theme['card-mod-card-yaml']
-	del original_theme['card-mod-row-yaml']
+	# Material Rounded themes
+	with open('./src/Material Rounded/material_rounded.yaml', 'r') as src:
+		# Create Material Rounded theme
+		theme_title = 'Material Rounded'
+		base_theme = yaml.load(src)[theme_title]
+		
+		output[theme_title] = copy.deepcopy(base_theme)
 
-	# Create a no mod version of theme with no card-mod
-	theme_title = 'Material Rounded No Mod'
-	theme_file.pop(theme_title, None)
-	theme_file[theme_title] = copy.deepcopy(original_theme)
-	del theme_file[theme_title]['card-mod-theme']
-	theme_file[theme_title]['app-header-background-color'] = 'var(--navbar-background)'
-	theme_file[theme_title]['primary-background-color'] = 'var(--lovelace-background)'
+		# Create a transparent card background version of theme
+		theme_title = 'Material Rounded Transparent Card'
+		transparent = 'var(--lovelace-background)'
+		output[theme_title] = copy.deepcopy(base_theme)
+		output[theme_title]['modes']['dark']['card-background-color'] = transparent
+		output[theme_title]['modes']['light']['card-background-color'] = transparent
+		output[theme_title]['ha-card-background'] = transparent
+		output[theme_title]['ha-card-border-color'] = transparent
 
+		# Create a no mod versions of theme with fixed status and navbar colors
+		theme_title = 'Material Rounded No Mod'
+		output[theme_title] = copy.deepcopy(output['Material Rounded'])
+		output[theme_title]['app-header-background-color'] = 'var(--navbar-background)'
+		output[theme_title]['primary-background-color'] = 'var(--lovelace-background)'
+		
+		theme_title = 'Material Rounded Transparent Card No Mod'
+		output[theme_title] = copy.deepcopy(output['Material Rounded Transparent Card'])
+		output[theme_title]['app-header-background-color'] = 'var(--navbar-background)'
+		output[theme_title]['primary-background-color'] = 'var(--lovelace-background)'
 
-	# Create a transparent card background version of theme
-	theme_title = 'Material Rounded Transparent Card'
-	transparent = 'var(--lovelace-background)'
-	theme_file.pop(theme_title, None)
-	theme_file[theme_title] = copy.deepcopy(original_theme)
-	theme_file[theme_title]['modes']['dark']['card-background-color'] = transparent
-	theme_file[theme_title]['modes']['light']['card-background-color'] = transparent
-	theme_file[theme_title]['ha-card-background'] = transparent
-	theme_file[theme_title]['ha-card-border-color'] = transparent
-	
-	# Create a transparent card background version of theme with no card-mod
-	theme_title = 'Material Rounded Transparent Card No Mod'
-	theme_file.pop(theme_title, None)
-	theme_file[theme_title] = copy.deepcopy(original_theme)
-	del theme_file[theme_title]['card-mod-theme']
-	theme_file[theme_title]['app-header-background-color'] = 'var(--navbar-background)'
-	theme_file[theme_title]['primary-background-color'] = 'var(--lovelace-background)'
-	theme_file[theme_title]['modes']['dark']['card-background-color'] = transparent
-	theme_file[theme_title]['modes']['light']['card-background-color'] = transparent
-	theme_file[theme_title]['ha-card-background'] = transparent
-	theme_file[theme_title]['ha-card-border-color'] = transparent
+		# Add card mod fields to main versions of theme
+		output['Material Rounded']['card-mod-theme'] = 'Material Rounded'
+		output['Material Rounded Transparent Card']['card-mod-theme'] = 'Material Rounded'
+		for element in ['root', 'card', 'row']:
+			with open(f'./src/Material Rounded/{element}.yaml', 'r') as css:
+				output['Material Rounded'][f'card-mod-{element}-yaml'] = css.read()
+				output['Material Rounded Transparent Card'][f'card-mod-{element}-yaml'] = output['Material Rounded'][f'card-mod-{element}-yaml']
 
-
-with open(theme_file_path, 'w') as f:
-	yaml.dump(theme_file, f)
+	yaml.dump(output, dist)
